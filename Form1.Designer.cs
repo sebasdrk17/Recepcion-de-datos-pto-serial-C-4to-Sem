45ec7d2ae7656b0864ccbbd34977aaf6e020159e
sebasdrk17 <47955967+sebasdrk17@users.noreply.github.com>
2019-06-14 23:35:38 -0500

    Subiendo archivos

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace puerto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listTemp = new System.Windows.Forms.ListBox();
+            this.btnSubirArchivo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(36, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar Comunicación";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Detener Comunicación";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listTemp
+            // 
+            this.listTemp.FormattingEnabled = true;
+            this.listTemp.Location = new System.Drawing.Point(36, 78);
+            this.listTemp.Name = "listTemp";
+            this.listTemp.Size = new System.Drawing.Size(222, 95);
+            this.listTemp.TabIndex = 3;
+            // 
+            // btnSubirArchivo
+            // 
+            this.btnSubirArchivo.Location = new System.Drawing.Point(112, 197);
+            this.btnSubirArchivo.Name = "btnSubirArchivo";
+            this.btnSubirArchivo.Size = new System.Drawing.Size(75, 23);
+            this.btnSubirArchivo.TabIndex = 4;
+            this.btnSubirArchivo.Text = "Subir a FTP";
+            this.btnSubirArchivo.UseVisualStyleBackColor = true;
+            this.btnSubirArchivo.Click += new System.EventHandler(this.btnSubirArchivo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(310, 246);
+            this.Controls.Add(this.btnSubirArchivo);
+            this.Controls.Add(this.listTemp);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Recepcion de datos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listTemp;
+        private System.Windows.Forms.Button btnSubirArchivo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Net;
+
+namespace puerto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SerialPort miPuerto;
+        int count = 0,archivosCreados=0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                miPuerto = new SerialPort("COM7", 9600, Parity.None, 8, StopBits.One);
+                miPuerto.Open();
+                timer1.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                miPuerto.Close();
+                timer1.Stop();
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string cad = "";
+            while (miPuerto.BytesToRead > 0)
+            {
+                cad = miPuerto.ReadExisting();
+                count++;
+                listTemp.Items.Add(cad);
+            }
+            if (count == 15)
+            {
+                crearArchivo();
+                count = 0;
+                archivosCreados++;
+            }
+        }
+        private void crearArchivo()
+        {
+            try
+            {
+                string nombreArchivo = "datos" + archivosCreados.ToString() + ".csv";
+                FileStream fs = new FileStream(nombreArchivo, FileMode.Create);
+                StreamWriter escritor = new StreamWriter(fs);
+                escritor.WriteLine(DateTime.Now.ToString());
+                for (int i = 0; i < listTemp.Items.Count; i++)
+                {
+                    escritor.WriteLine(listTemp.Items[i]);
+                }
+                escritor.Close();
+                fs.Close();
+                listTemp.Items.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSubirArchivo_Click(object sender, EventArgs e)
+        {
+            int indice = 0;
+            bool estado = false;
+            if(count!=10 && archivosCreados > 0)
+            {
+                archivosCreados--;
+            }
+            try
+            {
+                for (; indice <= archivosCreados; indice++)
+                {
+                    string rutaFTP = "ftp://" + "localhost";
+                    string rutaArchivo = "C:/Users/Alexis Hernandez/source/repos/puerto/puerto/bin/Debug/datos" + indice.ToString() + ".csv";
+                    string usuario = "pi";
+                    string contraseña = "pi";
+                    try
+                    {
+                        FtpWebRequest consulta = (FtpWebRequest)FtpWebRequest.Create(rutaFTP + "/" + Path.GetFileName(rutaArchivo));
+                        consulta.Method = WebRequestMethods.Ftp.UploadFile;
+                        consulta.Credentials = new NetworkCredential(usuario, contraseña);
+                        consulta.UsePassive = true;
+                        consulta.UseBinary = true;
+                        consulta.KeepAlive = false;
+                        FileStream cargar = File.OpenRead(rutaArchivo);
+                        byte[] buffer = new byte[cargar.Length];
+                        cargar.Read(buffer, 0, buffer.Length);
+                        cargar.Close();
+                        Stream streamQuery = consulta.GetRequestStream();
+                        streamQuery.Write(buffer, 0, buffer.Length);
+                        streamQuery.Close();
+                        estado = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        estado = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            if (estado)
+            {
+                MessageBox.Show("Archivos subidos exitosamente");
+            }
+        }
+    }
+}
